d483c761b8fae43006b47b78e65c935a0e4d0091
Matt Parmett <matt.parmett@gmail.com>
2021-06-25 15:41:37 -0700

    complete assignment 1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,7 +15,18 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        try:
+            donor = Donor.select().where(Donor.name == request.form['name']).get()
+        except Donor.DoesNotExist:
+            return render_template('create.jinja2', error="Error: donor does not exist.")
+        else:
+            Donation(value=request.form['amount'], donor=donor).save()
+            return redirect(url_for('all'))
+    return render_template('create.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
